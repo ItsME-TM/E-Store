1c15a2b235f45824aaf1aa1c1335b4fcbad856ba
TMbeast2000 <thushanmalaka123@gmail.com>
2024-05-24 21:27:28 +0530

    Create a method to hash the Password
    Here we created a function, when we create the user the password hash version add to the database. Also we create a method to compare the hashed password with user entered password in login

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose'); // Erase if already required
-
+const mongoose = require('mongoose'); 
+const bcrypt = require("bcrypt");
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstname:{
@@ -26,5 +26,13 @@ var userSchema = new mongoose.Schema({
     },
 });
 
+userSchema.pre("save", async function(next){
+        const salt = await bcrypt.genSaltSync(10);
+        this .password = await bcrypt.hashSync(this.password, salt);
+});
+
+userSchema.methods.isPasswordMarched = async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password);
+}
 //Export the model
 module.exports = mongoose.model('User', userSchema);
